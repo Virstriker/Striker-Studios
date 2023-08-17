4864455f855bd799b7320f7b0b5c40eb92be8204
vinayak  gohel <104017167+Virstriker@users.noreply.github.com>
2023-08-17 09:43:08 +0530

    .

---
+++ js/script.js
@@ -0,0 +1,100 @@
+$('.row1').owlCarousel({
+    rtl:false,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout: 3000,
+    dots:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1100:{
+            items:5
+        }
+    }
+})
+$('.row2').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    dots:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1100:{
+            items:5
+        }
+    }
+})
+$('.row3').owlCarousel({
+    rtl:false,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout: 5000,
+    dots:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1200:{
+            items:5
+        }
+    }
+})
+$('.row4').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout: 3000,
+    dots:false,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1200:{
+            items:5
+        }
+    }
+})
+$('.row5').owlCarousel({
+    rtl:false,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    dots:false,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1200:{
+            items:5
+        }
+    }
+})
